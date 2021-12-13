918019c707dbc453ed78b970ab278a0d051e5061
coder11235 <udaykalyansreenivasa@gmail.com>
2021-12-13 14:41:14 +0530

    changed gitognore

--- optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-74c98beb046ae250.d
+++
@@ -1,9 +0,0 @@
-/home/uday/cs/aoc-2021/optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-74c98beb046ae250.rmeta: src/main.rs src/day1.rs src/utils.rs src/day2.rs src/day3.rs
-
-/home/uday/cs/aoc-2021/optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-74c98beb046ae250.d: src/main.rs src/day1.rs src/utils.rs src/day2.rs src/day3.rs
-
-src/main.rs:
-src/day1.rs:
-src/utils.rs:
-src/day2.rs:
-src/day3.rs:
--- optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-db26fc17558cb871.d
+++
@@ -1,9 +0,0 @@
-/home/uday/cs/aoc-2021/optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-db26fc17558cb871: src/main.rs src/day1.rs src/utils.rs src/day2.rs src/day3.rs
-
-/home/uday/cs/aoc-2021/optimised_rust_solutions/target/debug/deps/optimised_rust_solutions-db26fc17558cb871.d: src/main.rs src/day1.rs src/utils.rs src/day2.rs src/day3.rs
-
-src/main.rs:
-src/day1.rs:
-src/utils.rs:
-src/day2.rs:
-src/day3.rs:
