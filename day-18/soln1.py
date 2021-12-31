975c4c068364d59076df3b23b23578204bd52a05
coder11235 <udaykalyansreenivasa@gmail.com>
2021-12-31 16:46:17 +0530

    made those functions member of the object in day 18

--- day-18/soln1.py
+++ day-18/soln1.py
@@ -2,6 +2,7 @@ inp = open('sample.txt', 'r').read().splitlines()
 import math
 
 class Node:
+    count = -1
     def __init__(self, node: 'Node' = None):
         self.parent = node
         self.left = None
@@ -51,6 +52,56 @@ class Node:
         vals['idhash'] = hex(id(self))
         return str(vals)
 
+    def reduce_num(self):
+        Node.count += 1
+        if not isinstance(self, int):
+            if Node.count == 4:
+                self.explode()
+            elif isinstance(self.left, int) and self.left >= 10:
+                newnode = Node()
+                newnode.left = self.left//2
+                newnode.right = math.ceil(self.left/2)
+                newnode.parent = self
+                self.left = newnode
+            elif isinstance(self.right, int) and self.right >= 10:
+                newnode = Node()
+                newnode.left = self.right//2
+                newnode.right = math.ceil(self.right/2)
+                newnode.parent = self
+                self.right = newnode
+
+        if not isinstance(self.left, int):
+            self.left.reduce_num()
+        if not isinstance(self.right, int):
+            self.right.reduce_num()
+        Node.count -= 1
+
+    def explode(self):
+        right = self.right
+        left= self.left
+
+        # add to rightmode element
+        right_par = self.get_right_par()
+        if right_par is not None:
+            if isinstance(right_par.right, int):
+                right_par.right += right
+            else:
+                cp = right_par.right.get_leftest_child_parent()
+                cp.left += right
+
+        left_par = self.get_left_par()
+        if left_par is not None:
+            if isinstance(left_par.left, int):
+                left_par.left += left
+            else:
+                cp = left_par.left.get_rightest_child_parent()
+                cp.right += left
+
+        if self.parent.right == self:
+            self.parent.right = 0
+        else:
+            self.parent.left = 0
+
     def debugprint(self, pr = False):
         print('[', end='')
         if isinstance(self.left, int):
@@ -79,58 +130,6 @@ def parse(line: str):
             current_node.set(finished)
     return current_node
 
-def explode(node: Node):
-    right = node.right
-    left= node.left
-
-    # add to rightmode element
-    right_par = node.get_right_par()
-    if right_par is not None:
-        if isinstance(right_par.right, int):
-            right_par.right += right
-        else:
-            cp = right_par.right.get_leftest_child_parent()
-            cp.left += right
-
-    left_par = node.get_left_par()
-    if left_par is not None:
-        if isinstance(left_par.left, int):
-            left_par.left += left
-        else:
-            cp = left_par.left.get_rightest_child_parent()
-            cp.right += left
-
-    if node.parent.right == node:
-        node.parent.right = 0
-    else:
-        node.parent.left = 0
-
-count = -1
-def reduce_num(node: Node):
-    global count
-    count += 1
-    if not isinstance(node, int):
-        if count == 4:
-            explode(node)
-        elif isinstance(node.left, int) and node.left >= 10:
-            newnode = Node()
-            newnode.left = node.left//2
-            newnode.right = math.ceil(node.left/2)
-            newnode.parent = node
-            node.left = newnode
-        elif isinstance(node.right, int) and node.right >= 10:
-            newnode = Node()
-            newnode.left = node.right//2
-            newnode.right = math.ceil(node.right/2)
-            newnode.parent = node
-            node.right = newnode
-
-    if not isinstance(node.left, int):
-        reduce_num(node.left)
-    if not isinstance(node.right, int):
-        reduce_num(node.right)
-    count -= 1
-
 def propparse(line: str):
     node: Node = parse(line)
     node = node.left
@@ -148,16 +147,16 @@ def add(node1: Node, node2: Node):
 first = inp.pop(0)
 first = propparse(first)
 for _ in range(5):
-    reduce_num(first)
+    first.reduce_num()
 
 for i in inp:
     node = propparse(i)
     for _ in range(40):
-        reduce_num(node)
-        reduce_num(first)
+        node.reduce_num()
+        first.reduce_num()
     first = add(first, node)
     for _ in range(40):
-        reduce_num(node)
-        reduce_num(first)
+        node.reduce_num()
+        first.reduce_num()
 
 first.debugprint()
