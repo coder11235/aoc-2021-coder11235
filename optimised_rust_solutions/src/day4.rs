9a1d034cbc0af0f47e4effbe2d41af5bde10c8c4
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-01 13:47:50 +0530

    created an optimised rust solution for day 4

--- optimised_rust_solutions/src/day4.rs
+++ optimised_rust_solutions/src/day4.rs
@@ -1,23 +1,30 @@
 pub fn part1(input: &String) -> usize {
-    parse(input);
-    5
+    let (numbers, boards) = parse(input);
+    let mut checking_array = construct_checked_array(boards.len());
+    for number in numbers {
+        for bnum in 0..boards.len() {
+            check_number(&boards[bnum], number, &mut checking_array[bnum]);
+            if check_for_horizontal_win(&checking_array[bnum]) || check_for_vertical_win(&checking_array[bnum]) {
+                return find_sum(&boards[bnum], &checking_array[bnum], number);
+            }
+        }
+    }
+    return 5;
 }
 
-pub fn parse(input: &String) -> () {
+pub fn parse(input: &String) -> (Vec<u8>, Vec<Vec<Vec<u8>>>) {
     let mut boards = input.split("\n\n");
-    let numbers: Vec<i32> = boards.next().unwrap()
-    .split(',').map(|chr| chr.parse().unwrap()).collect();
-    let boards: Vec<&str> = boards
-    .map(|board| {
+    let numbers: Vec<u8> = boards.next().unwrap()
+    .split(',').map(|chr| chr.parse::<u8>().unwrap()).collect();
+    let boards = boards.map(|board| {
         board.lines().map(|row| {
-            row.replace("  ", " ").split(" ")
-        })
-    })
-    .collect();
-    println!("{:?}\n{:?}", boards, numbers);
+            row.split(" ").filter(|x | *x != "").map(|x| x.parse::<u8>().unwrap()).collect::<Vec<u8>>()
+        }).collect::<Vec<Vec<u8>>>()
+    }).collect::<Vec<Vec<Vec<u8>>>>();
+    return (numbers, boards)
 }
 
-pub fn construct_checked_array(len: usize) -> Vec<Vec<Vec<bool>>> {
+fn construct_checked_array(len: usize) -> Vec<Vec<Vec<bool>>> {
     let mut boardslist = Vec::new();
     for _ in 0..len {
         let mut board = Vec::new();
@@ -29,7 +36,7 @@ pub fn construct_checked_array(len: usize) -> Vec<Vec<Vec<bool>>> {
     return boardslist;
 }
 
-pub fn check_number(board: &Vec<Vec<i32>>, number: i32, checked_array: &mut Vec<Vec<bool>>) {
+fn check_number(board: &Vec<Vec<u8>>, number: u8, checked_array: &mut Vec<Vec<bool>>) {
     for i in 0..5 {
         for j in 0..5 {
             if board[i][j] == number {
@@ -40,7 +47,7 @@ pub fn check_number(board: &Vec<Vec<i32>>, number: i32, checked_array: &mut Vec<
     }
 }
 
-pub fn check_for_horizontal_win(checked_array: Vec<Vec<bool>>) -> bool {
+fn check_for_horizontal_win(checked_array: &Vec<Vec<bool>>) -> bool {
     for i in checked_array {
         for j in i {
             if !j {
@@ -51,7 +58,7 @@ pub fn check_for_horizontal_win(checked_array: Vec<Vec<bool>>) -> bool {
     return true;
 }
 
-pub fn check_for_vertical_win(checked_array: Vec<Vec<bool>>) -> bool {
+fn check_for_vertical_win(checked_array: &Vec<Vec<bool>>) -> bool {
     for i in 0..5 {
         let mut won = true;
         for j in 0..5 {
@@ -64,4 +71,16 @@ pub fn check_for_vertical_win(checked_array: Vec<Vec<bool>>) -> bool {
         }
     }
     return false;
+}
+
+fn find_sum(board: &Vec<Vec<u8>>, checking_array: &Vec<Vec<bool>>, number: u8) -> usize {
+    let mut sum = 0;
+    for i in 0..5 {
+        for j in 0..5 {
+            if checking_array[i][j] == false {
+                sum += board[i][j] as usize;
+            }
+        }
+    }
+    return sum * number as usize;
 }
