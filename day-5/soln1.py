90923a29cccbece530a9f8fd67b9537d29044af4
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-01 23:00:38 +0530

    made major optimisation in day 5 python version

--- day-5/soln1.py
+++ day-5/soln1.py
@@ -1,7 +1,14 @@
 file = open('data.txt', 'r')
 data = file.read().split('\n')
 
-points = []
+points = {}
+
+def ins_point(x, y):
+    global points
+    if (x,y) in points:
+        points[(x,y)] += 1
+    else:
+        points[(x,y)] = 1
 
 for i in data:
     left, right = i.split('->')
@@ -17,31 +24,22 @@ for i in data:
     if lx == rx:
         if ly <= ry:
             for i in range(ly, ry + 1):
-                temppoints.append([rx, i])
-        elif ly >= ry:
-            for i in range(ry, ly + 1):
-                temppoints.append([rx, i])
+                ins_point(rx, i)
         else:
-            temppoints.append([rx, ry])
+            for i in range(ry, ly + 1):
+                ins_point(rx, i)
     elif ly == ry:
         if lx <= rx:
             for i in range(lx, rx + 1):
-                temppoints.append([i, ry])
-        elif lx >= rx:
-            for i in range(rx, lx + 1):
-                temppoints.append([i, ry])
+                ins_point(i, ry)
         else:
-            temppoints.append([rx, ry])
-    points += temppoints
+            for i in range(rx, lx + 1):
+                ins_point(i, ry)
 
-print(points)
-
-firstencountered = []
 count = 0
-for i in points:
-    if firstencountered.count(i) == 1:
+
+for val in points.values():
+    if val >= 2:
         count += 1
-        print(count)
-    firstencountered.append(i)
 
 print(count)
