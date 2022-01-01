77ac5e540e18c4a49b6244abfee717fd4407249f
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-01 15:39:30 +0530

    did prep for starting day 5

--- optimised_rust_solutions/src/main.rs
+++ optimised_rust_solutions/src/main.rs
@@ -3,6 +3,7 @@ mod utils;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 use std::fs;
 
@@ -10,6 +11,6 @@ fn main() {
     let content = fs::read_to_string("inputs/day4.txt")
         .expect("could not read file");
     let time = std::time::Instant::now();
-    let ans = day4::part2(&content);
-    println!("day 4 soln: {} finished in: {:?}", ans, time.elapsed());
+    let ans = day5::part1(&content);
+    println!("day 5 soln: {} finished in: {:?}", ans, time.elapsed());
 }
