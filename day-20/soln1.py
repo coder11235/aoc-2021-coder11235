1596a111182551b50056b355263bfbe866145df7
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-05 22:55:23 +0530

    made a bit of day 20

--- day-20/soln1.py
+++ day-20/soln1.py
@@ -1,4 +1,4 @@
-inp = open('sample.txt', 'r').read()
+inp = open('data.txt', 'r').read()
 
 def parse(inp: str):
     alg, img = inp.split('\n\n')
@@ -6,4 +6,42 @@ def parse(inp: str):
     imgarr = [[True if i == '#' else False for i in ln] for ln in img.splitlines()]
     return alg, imgarr
 
+def debug_prnt_img(inpimg):
+    for i in inpimg:
+        for j in i:
+            print('# ' if j else '. ', end='')
+        print()
+
+def append_on_all_sides(inpimp):
+    for ri in range(len(inpimg)):
+        inpimg[ri].insert(0, False)
+        inpimg[ri].append(False)
+
+    inpimg.append([False]*len(inpimg[0]))
+    inpimg.insert(0, [False]*len(inpimg[0]))
+
+def get_index(coord, inpimg):
+    adj = ""
+    for a in [-1,0,1]:
+        for b in [-1,0,1]:
+            adj += '1' if inpimg[coord[0]+a][coord[1]+b] else '0'
+    return int(adj, 2)
+
 alg, inpimg = parse(inp)
+
+for num in range(2):
+    append_on_all_sides(inpimg)
+    append_on_all_sides(inpimg)
+    newimg = []
+    for i in range(1, len(inpimg)-1):
+        newrow = []
+        for j in range(1, len(inpimg[0])-1):
+            algin = get_index((i, j), inpimg)
+            newrow.append(alg[algin])
+        newimg.append(newrow)
+    inpimg = newimg
+
+
+debug_prnt_img(inpimg)
+count = sum([sum(row) for row in inpimg])
+print(count)
