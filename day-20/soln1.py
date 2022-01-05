dd06c1f5df5b67c3cf774fdf0388db1f9abd936a
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-06 01:17:15 +0530

    finished day 20 part 1

--- day-20/soln1.py
+++ day-20/soln1.py
@@ -1,4 +1,4 @@
-inp = open('sample.txt', 'r').read()
+inp = open('data.txt', 'r').read()
 
 def parse(inp: str):
     alg, img = inp.split('\n\n')
@@ -13,17 +13,13 @@ def debug_prnt_img(inpimg):
         print()
 
 def append_on_all_sides(inpimp):
-    def append_on_lr(inpimg):
-        for ri in range(len(inpimg)):
-            inpimg[ri].insert(0, False)
-            inpimg[ri].append(False)
-    def append_on_bt(inpimg):
-        inpimg.append([False]*len(inpimg[0]))
-        inpimg.insert(0, [False]*len(inpimg[0]))
-    append_on_lr(inpimg)
-    append_on_bt(inpimg)
+    for ri in range(len(inpimg)):
+        inpimg[ri].insert(0, False)
+        inpimg[ri].append(False)
+    inpimg.insert(0, [False]*len(inpimg[0]))
+    inpimg.append([False]*len(inpimg[0]))
 
-def get_digit(coord, inpimg):
+def get_index(coord, inpimg):
     adj = ""
     for a in [-1,0,1]:
         for b in [-1,0,1]:
@@ -33,21 +29,19 @@ def get_digit(coord, inpimg):
 alg, inpimg = parse(inp)
 
 append_on_all_sides(inpimg)
+append_on_all_sides(inpimg)
+append_on_all_sides(inpimg)
+append_on_all_sides(inpimg)
 
-for num in range(5):
-    append_on_all_sides(inpimg)
-    append_on_all_sides(inpimg)
+for _ in range(2):
     newimg = []
     for i in range(1, len(inpimg)-1):
         newrow = []
         for j in range(1, len(inpimg[0])-1):
-            algin = get_digit((i, j), inpimg)
+            algin = get_index((i, j), inpimg)
             newrow.append(alg[algin])
         newimg.append(newrow)
     inpimg = newimg
     debug_prnt_img(inpimg)
-    print(sum([sum(row) for row in inpimg]))
-    print()
 
-# count = sum([sum(row) for row in inpimg])
-# print(count)
+print(sum([sum(row) for row in inpimg]))
