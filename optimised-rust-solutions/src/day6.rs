665945e879bad21650d9c7f92d43a5434488f710
coder11235 <udaykalyansreenivasa@gmail.com>
2022-01-20 09:31:29 +0530

    added rustic day6

---
+++ optimised-rust-solutions/src/day6.rs
@@ -0,0 +1,30 @@
+pub fn part1(input: &String) -> usize{
+    let mut fishes = parse(input);
+    for _ in 0..80 {
+        perform_gen(&mut fishes)
+    }
+    fishes.iter().sum()
+}
+
+pub fn part2(input: &String) -> usize{
+    let mut fishes = parse(input);
+    for _ in 0..256 {
+        perform_gen(&mut fishes)
+    }
+    fishes.iter().sum()
+}
+
+fn perform_gen(fishes: &mut Vec<usize>) {
+    let newfishes = fishes.remove(0);
+    fishes[6] += newfishes;
+    fishes.push(newfishes);
+}
+
+fn parse(input: &String) -> Vec<usize> {
+    let mut counts = vec![0; 9];
+    for i in input.split(',') {
+        let parsed = i.parse::<usize>().unwrap();
+        counts[parsed] += 1;
+    }
+    return counts;
+}
--- optimised-rust-solutions/src/main.rs
+++ optimised-rust-solutions/src/main.rs
@@ -5,13 +5,14 @@ mod day3;
 mod day4;
 mod day5;
 mod day21;
+mod day6;
 
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("inputs/day5.txt")
+    let content = fs::read_to_string("inputs/day6.txt")
         .expect("could not read file");
     let time = std::time::Instant::now();
-    let ans = day5::part1(&content);
-    println!("day 21 soln: {} finished in: {:?}", ans, time.elapsed());
+    let ans = day6::part2(&content);
+    println!("day 6 soln: {} finished in: {:?}", ans, time.elapsed());
 }
