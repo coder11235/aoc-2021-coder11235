d70809dab104c0c0307ff9c92a6a24c2cedc267a
uday kalyan sreenivasa <udaykalyansreenivasa@gmail.com>
2022-01-31 18:07:54 +0530

    post merge stuff

--- optimised-rust-solutions/src/main.rs
+++ optimised-rust-solutions/src/main.rs
@@ -4,7 +4,6 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
-mod day21;
 mod day6;
 
 use std::fs;
@@ -13,6 +12,6 @@ fn main() {
     let content = fs::read_to_string("inputs/day21.txt")
         .expect("could not read file");
     let time = std::time::Instant::now();
-    let ans = day21::part2(&content);
+    let ans = day2::part2(&content);
     println!("day 6 soln: {} finished in: {:?}", ans, time.elapsed());
 }
