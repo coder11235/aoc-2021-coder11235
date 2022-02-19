c80434465942946d9691a88fc0fe54b35f2db4e5
coder11235 <udaykalyansreenivasa@gmail.com>
2022-02-19 18:31:57 +0530

    updated soln1

--- solutions/day-19/soln1.py
+++ solutions/day-19/soln1.py
@@ -1,29 +1,57 @@
+from functools import cache
 inp = open('sample.txt','r').read()
 
-def parse_scanner(scannertxt: str):
-    lines = scannertxt.splitlines()
-    parsedcoords = []
-    for i in lines[1:]:
-        parsedcoords.append(tuple([int(j) for j in i.split(',')]))
-    return parsedcoords
+"""
+schema(?) for each scanner
+scanners
+    - scanner
+        - orientation reversing function
+        - beacons
+            beacon
+                - all beacons relative to it
+"""
 
-scanners = [parse_scanner(i) for i in inp.split('\n\n')]
-
-scanners = [scanners[0]]
-
-rots = [
-    lambda a: (a[0], a[1], a[2]),
-    lambda a: (a[2], a[0], a[1]),
-    lambda a: (a[1], a[2], a[0]),
+negative_functions = [
+    lambda a: [-a[0], a[1], a[2]],
+    lambda a: [a[0], -a[1], a[2]],
+    lambda a: [a[0], a[1], -a[2]],
+    lambda a: [-a[0], -a[1], a[2]],
+    lambda a: [a[0], -a[1], -a[2]],
+    lambda a: [-a[0], a[1], -a[2]],
+    lambda a: [-a[0], -a[1], -a[2]],
+    lambda a: [a[0], a[1], a[2]],
 ]
 
-negtrans = [
-    lambda a: (-a[0], a[1], a[2]),
-    lambda a: (a[0], -a[1], a[2]),
-    lambda a: (a[0], a[1], -a[2]),
-    lambda a: (-a[0], -a[1], a[2]),
-    lambda a: (a[0], -a[1], -a[2]),
-    lambda a: (-a[0], a[1], -a[2]),
-    lambda a: (-a[0], -a[1], -a[2]),
-    lambda a: (a[0], a[1], a[2]),
+rotation_functions = [
+    (
+        lambda a: [a[0], a[1], a[2]],
+        lambda a: [a[0], a[1], a[2]],
+    ),
+    (
+        lambda a: [a[2], a[0], a[1]],
+        lambda a: [a[1], a[2], a[0]],
+    ),
+    (
+        lambda a: [a[1], a[2], a[0]],
+        lambda a: [a[2], a[0], a[1]],
+    )
 ]
+
+@functools.cache
+def orientations():
+    """
+    gets the 24 orientation functions (must be cached)
+
+    Returns:
+    a list having 2 element lists in the format [orientation function: reversing function]
+    """
+    for negative_func in negative_functions:
+        for rotation_func, reverse_rotation_func in rotation_functions:
+            yield [
+                lambda point: negative_func(rotation_func(point)),
+                lambda point: reverse_rotation_func(negative_func(point))
+            ]
+
+for i in range(5):
+    for tr, rev in orientations():
+        print(tr([1,2,3]), rev(tr([1,2,3])))
