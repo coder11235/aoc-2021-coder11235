c5fa9e846d3f1f3fe325fac14c66835aa89756ca
coder11235 <udaykalyansreenivasa@gmail.com>
2022-04-08 02:04:45 +0530

    finished opt day 8

--- optimised-rust-solutions/src/main.rs
+++ optimised-rust-solutions/src/main.rs
@@ -8,13 +8,14 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("inputs/day7.txt")
+    let content = fs::read_to_string("inputs/day8.txt")
         .expect("could not read file");
     let time = std::time::Instant::now();
-    let ans = day7::part1(&content);
+    let ans = day8::part2(&content);
     println!("day soln: {} finished in: {:?}", ans, time.elapsed());
 }
