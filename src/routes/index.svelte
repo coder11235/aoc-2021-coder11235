ffe0539867d5fe161084a2d673a21ef07090a968
uday kalyan sreenivasa <udaykalyansreenivasa@gmail.com>
2022-07-23 02:01:21 +0530

    added basic layout-ey stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" type="ts">
+	export async function load({ fetch }) {
+		fetch()
+	}
+</script>
+
+<script type="ts">
+	import tree from '$lib/assets/tree.jpg'
+</script>
+
+<main>
+	<div class="heading">
+		My 2021 <br>
+		Advent of Code <br>
+		Solutions <br>
+	</div>
+	<img src="{tree}" alt="christmas tree pic" width="100%">
+	<div class="section">
+		<div class="subheading">
+			<span>optimized rust solution performances</span>
+		</div>
+	</div>
+</main>
+
+<style>
+	.subheading {
+		color: white;
+		font-size: 60px;
+		display: flex;
+		justify-content: center;
+	}
+	main {
+		background-color: #00001A;
+	}
+	.heading {
+		position: absolute;
+		top: 30%;
+		left: 13%;
+		font-size: 85px;
+		color: beige;
+	}
+	.section {
+		margin-top: 7%;
+	}
+</style>
