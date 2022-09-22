554f37ebc5e99185e2b427e5736439b82d390ad2
uday kalyan sreenivasa <udaykalyansreenivasa@gmail.com>
2022-09-22 12:51:12 +0530

    idk what changes i made here

---
+++ src/routes/solutions/[day].svelte
@@ -0,0 +1,7 @@
+<script context="module" type="ts">
+	import type { ExternalFetch, Page } from "@sveltejs/kit";
+	export async function load({fetch, page}: {fetch: ExternalFetch, page: Page}){
+		let day = page.params.day;
+		
+	}
+</script>
